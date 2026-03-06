e1eab0d14d79bcf350442c0aea96ad0f9c2e4b71
unknown <dagayash14@gmail.com>
2026-03-06 22:06:35 +0530

    made the save and retrive for the message and thier embeddings

--- helper/session.py
+++ helper/session.py
@@ -1,5 +1,6 @@
 #CRUD operations for chat sessions
 from db import db
+from embeddings import get_embeddings
 
 def save_session(user_id:int, conversation_id:str, metadata:dict):
     """
@@ -43,8 +44,6 @@ def load_messages(session_id):
     """
     return 1
 
-
-
 def load_global_persona(user_id:int):
     """
     Load the global persona for the user from the database using the user_id
@@ -61,3 +60,31 @@ def save_global_persona(user_id, persona):
     params = (persona, user_id,)
     print(params)
     return db.execute(query, params)
+
+def save_message_n_message_embeddings(conversation_id, role, content):
+    query = "INSERT INTO messages (conversation_id, role, content) VALUES (?, ?, ?)"
+    params = (conversation_id, role, content,)
+    cursor = db.execute(query, params)
+
+    message_id = cursor.lastrowid
+    embedding = get_embeddings(content)
+
+    query = "INSERT INTO message_embeddings VALUES (?, ?)"
+    params = (message_id, embedding.tobytes(),)
+    cursor = db.execute(query, params)
+
+    return None
+
+def retrive_context(message: str, top_k: int) -> list:
+    """
+    MATCH ? is how sqlite-vec triggers KNN search
+    AND K = ? replaces the LIMIT ?
+    ORDER BY distance BY uses cosine distance when using float vectors
+    """
+
+    query_embeddings = get_embeddings(message)
+    query = f"""SELECT messages.content FROM message_embeddings JOIN messages ON messages.message_id = message_embeddings.message_id_ref WHERE message_embeddings.embedding MATCH ? AND k = {top_k} ORDER BY distance ASC;"""
+    params = (query_embeddings.tobytes(),)
+    rows = db.query(query, params)
+
+    return [r["content"] for r in rows]
